73833563c8b8dc1e56c6e48956498a2daaa39cd1
Jukka Svahn <web@rahinaa.biz>
2012-10-04 03:49:50 +0300

    Consistent inline comments.

--- textpattern/lib/txplib_update.php
+++ textpattern/lib/txplib_update.php
@@ -44,18 +44,18 @@
 				while (!feof($file))
 				{
 					$line = fgets($file, 4096);
-					# ignore empty lines and simple comments (any line starting with #, not followed by @)
+					// ignore empty lines and simple comments (any line starting with #, not followed by @)
 					if (trim($line) === '' || ($line[0] == '#' && $line[1] != '@' && $line[1] != '#'))
 					{
 						continue;
 					}
-					# if available use the lastmod time from the file
+					// if available use the lastmod time from the file
 					if (strpos($line,'#@version') === 0)
 					{	# Looks like: "#@version id;unixtimestamp"
 						@list($fversion,$ftime) = explode(';',trim(substr($line,strpos($line,' ',1))));
 						$lastmod = date("YmdHis",min($ftime, time()));
 					}
-					# each language section should be prefixed by #@
+					// each language section should be prefixed by #@
 					if ($line[0] == '#' && $line[1] == '@')
 					{
 						if (!empty($data))
@@ -80,7 +80,7 @@
 								}
 							}
 						}
-						# reset
+						// reset
 						$data = array();
 						$event = substr($line,2, (strlen($line)-2));
 						$event = rtrim($event);
@@ -91,15 +91,15 @@
 						continue;
 					}
 
-					# Guard against setup strings being loaded.
-					# TODO: Setup strings will be removed from the .txt files at some point; this check can then be removed
+					// Guard against setup strings being loaded.
+					// TODO: Setup strings will be removed from the .txt files at some point; this check can then be removed
 					if ($event !== 'setup')
 					{
 						@list($name,$val) = explode(' => ',trim($line));
 						$data[$name] = $val;
 					}
 				}
-				# remember to add the last one
+				// remember to add the last one
 				if (!empty($data))
 				{
 					foreach ($data as $name => $value)
@@ -109,7 +109,7 @@
 				}
 				mysql_query("DELETE FROM `".PFX."txp_lang`  WHERE `lang`='".$lang."' AND `event` IN ('".join("','", array_unique($core_events))."') AND `lastmod`>$lastmod");
 				@fclose($filename);
-				#delete empty fields if any
+				// delete empty fields if any
 				mysql_query("DELETE FROM `".PFX."txp_lang` WHERE `data`=''");
 				mysql_query("FLUSH TABLE `".PFX."txp_lang`");
 
