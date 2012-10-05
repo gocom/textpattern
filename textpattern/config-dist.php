062a2a5c2465a274086e80e9c8f3d5633d734017
Jukka Svahn <web@rahinaa.biz>
2012-10-05 11:47:05 +0300

    phpdoc the $txpcfg config.

--- textpattern/config-dist.php
+++ textpattern/config-dist.php
@@ -1,53 +1,84 @@
 <?php
 
+/**
+ * Database credentials and site configuration.
+ * @package DB
+ */
+
 	/**
-	 *  mysql database
+	 * Mysql database.
+	 *
+	 * @global string $txpcfg['db']
 	 */
+
 		$txpcfg['db'] = 'databasename';
 
 	/**
-	 *  database login name
+	 * Database login name.
+	 *
+	 * @global string $txpcfg['user']
 	 */
 		$txpcfg['user'] = 'root';
 
 	/**
-	 *  database password
+	 * Database password.
+	 *
+	 * @global string $txpcfg['pass']
 	 */
 		$txpcfg['pass'] = '';
 
 	/**
-	 *  database host
+	 * Database hostname.
+	 *
+	 * @global string $txpcfg['host']
 	 */
 
 		$txpcfg['host'] = 'localhost';
 
 	/**
-	 *  table prefix (Use ONLY if you require multiple installs in one db)
+	 * Table prefix.
+	 *
+	 * Use ONLY if you require multiple installs in one db.
+	 *
+	 * @global string $txpcfg['table_prefix']
 	 */
 
 		$txpcfg['table_prefix'] = '';
 
 	/**
-	 *  full server path to textpattern dir (no slash at end)
+	 * Full server path to textpattern directory, no slash at end.
+	 *
+	 * @global string $txpcfg['txpath']
 	 */
 
 		$txpcfg['txpath'] = '/home/path/to/textpattern';
 
 	/**
-	 *  DB Connection Charset, only for MySQL4.1 and up. Must be equal to the Table-Charset.
+	 * DB Connection Charset.
+	 *
+	 * Only for MySQL4.1 and up. Must be equal to the Table-Charset, e.g. latin1 or utf8.
+	 *
+	 * @global string $txpcfg['dbcharset']
 	 */
 
 		$txpcfg['dbcharset'] = 'latin1';
 
 	/**
-	 *  optional: database client flags as needed (@see http://www.php.net/manual/function.mysql-connect.php)
+	 * Database client flags.
+	 *
+	 * These are optional. Use the database client flags as needed.
+	 * Flags: MYSQL_CLIENT_SSL, MYSQL_CLIENT_COMPRESS, MYSQL_CLIENT_IGNORE_SPACE,
+	 * MYSQL_CLIENT_INTERACTIVE
+	 *
+	 * @global int $txpcfg['client_flags']
+	 * @link   http://www.php.net/manual/function.mysql-connect.php
 	 */
 
 	#	$txpcfg['client_flags'] = MYSQL_CLIENT_SSL | MYSQL_CLIENT_COMPRESS;
 
-	/**
-	 *  optional, advanced: http address of the site serving images
-	 *  @see http://forum.textpattern.com/viewtopic.php?id=34493
+	/*
+	 * optional, advanced: http address of the site serving images
+	 * see http://forum.textpattern.com/viewtopic.php?id=34493
 	 */
 
 	# define('ihu', 'http://static.example.com/');
