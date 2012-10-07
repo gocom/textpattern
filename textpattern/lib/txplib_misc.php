d9a12cd2172e4f522aa769667d8c0a83aca400fe
Jukka Svahn <web@rahinaa.biz>
2012-10-07 08:54:12 +0300

    Moved eE().

--- textpattern/lib/txplib_misc.php
+++ textpattern/lib/txplib_misc.php
@@ -2479,6 +2479,22 @@
 	}
 
 /**
+ * Converts an email address into unicode entities.
+ *
+ * @param   string $txt The email address
+ * @return  string HTML entities
+ * @package Email
+ */
+
+	function eE($txt)
+	{
+		for ($i=0;$i<strlen($txt);$i++) {
+			$ent[] = "&#".ord(substr($txt,$i,1)).";";
+		}
+		if (!empty($ent)) return join('',$ent);
+	}
+
+/**
  * Replaces CRLF with two spaces.
  *
  * @param   string $str The string
--- textpattern/publish/taghandlers.php
+++ textpattern/publish/taghandlers.php
@@ -785,22 +785,6 @@
 		return $link;
 	}
 
-/**
- * Converts an email address into unicode entities.
- *
- * @param   string $txt The email address
- * @return  string HTML entities
- * @package Email
- */
-
-	function eE($txt)
-	{
-		for ($i=0;$i<strlen($txt);$i++) {
-			$ent[] = "&#".ord(substr($txt,$i,1)).";";
-		}
-		if (!empty($ent)) return join('',$ent);
-	}
-
 // -------------------------------------------------------------
 
 	register_tag('email');
