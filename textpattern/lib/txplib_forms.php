gocom/textpattern

commit 9c339225cb0fba13528d7ab9a8562be991da65f2

selectInput(): Coerce types of selected value and of options.
git-svn-id: http://textpattern.googlecode.com/svn/development/4.x@4113 2fea6a4d-a838-0410-917b-93a53c48e9d2

textpattern/lib/txplib_forms.php
@@ -65,6 +65,11 @@ $LastChangedRevision$
 			}
 
 			else {
+				// coerce type of array members into incoming value's type
+				// otherwise ('foo' == 0) === true
+				settype($avalue, gettype($value));
+				settype($alabel, gettype($value));
+
 				if ($avalue == $value || $alabel == $value) {
 					$sel = ' selected="selected"';
 					$selected = true;

commit f3a7a583f29f1ee0cc755089ed76ad8b21424d62

Fixed selectInput() issues caused by r4113.

textpattern/lib/txplib_forms.php
@@ -93,35 +93,14 @@
 
 		foreach ($array as $avalue => $alabel)
 		{
-			if ($check_type)
+			if ((string) $avalue === (string) $value || (string) $alabel === (string) $value)
 			{
-				if ($avalue === $value || $alabel === $value)
-				{
-					$sel = ' selected="selected"';
-					$selected = true;
-				}
-				else
-				{
-					$sel = '';
-				}
+				$sel = ' selected="selected"';
+				$selected = true;
 			}
-
 			else
 			{
-				// coerce type of array members into incoming value's type
-				// otherwise ('foo' == 0) === true
-				settype($avalue, gettype($value));
-				settype($alabel, gettype($value));
-
-				if ($avalue == $value || $alabel == $value)
-				{
-					$sel = ' selected="selected"';
-					$selected = true;
-				}
-				else
-				{
-					$sel = '';
-				}
+				$sel = '';
 			}
 
 			$out[] = n.t.'<option value="'.txpspecialchars($avalue).'"'.$sel.'>'.txpspecialchars($alabel).'</option>';